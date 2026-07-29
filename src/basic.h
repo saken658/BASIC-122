b160ebeb496673668fef0b2ea39e070801479cb6
Scott Kenney <rmta658@yahoo.com>
2026-07-28 22:29:29 -0400

    Revert "Saken658 psychic robot"

--- src/basic.h
+++ src/basic.h
@@ -9,7 +9,9 @@
  ****************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#ifndef SOLARIS
 #include <stdint.h>
+#endif
 #include <string.h>
 #include <strings.h>
 #include <unistd.h>
